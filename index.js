a76fbb16557f934b8f333a830e6718727ad4cf07
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-01 09:55:26 -0500

    finished lab

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+const square = (number) => {
+    return number * number
+}
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
